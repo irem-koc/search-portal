3b30b5c094925762a123cb2ef6e1eb2e4bf600fa
irem-koc <irem_koc_75@hotmail.com>
2023-05-07 17:15:07 +0300

    added some styles to landing page css file

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,21 +2,38 @@ import React from "react";
 import "./Landing.css";
 import { Link } from "react-router-dom";
 import logo from "./../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot} from "@fortawesome/free-solid-svg-icons";
+
 const Landing = () => {
     return (
         <div>
-            <button className="add-btn">
-                <Link to="/addlink">Add new record</Link>
-            </button>
-            <figure>
-                <img src={logo} alt="logo" />
-                <figcaption>Search app</figcaption>
-            </figure>
-            <h2>Find in records</h2>
-            <input type="text" />
-            <button>Search</button>
-            <Link to="/showmore">Show more...</Link>
-            <h2>Top News</h2>
+            <div className="landing-page">
+                <div className="add-btn-section">
+                    <button className="add-btn">
+                        <Link to="/addlink">Add new record</Link>
+                    </button>
+                </div>
+                <div className="logo-section">
+                    <figure className="figure">
+                        <img className="logo" src={logo} alt="logo" />
+                        <figcaption className="alt-text-logo">Search app</figcaption>
+                    </figure>
+                </div>
+                <div className="find-input">
+                    <div className="find-record">Find in records</div>
+                    <div className="input-seacrh-btn-group">
+                        <form className="input-section">
+                            <button type="submit">Search</button>
+                            <input className="search-input" type="search" />
+                        </form>
+                        <button className="search-btn">Search</button>
+                    </div>
+                    {/* <Link to="/showmore">Show more...</Link> */}
+                </div>
+                <h2>Top News</h2>
+                <FontAwesomeIcon icon={faLocationDot} />
+            </div>
         </div>
     );
 };
